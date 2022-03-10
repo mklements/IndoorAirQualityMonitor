36506d72d1fa1450104366f5281b81cbe220d195
Michael Klements <82847422+mklements@users.noreply.github.com>
2022-03-10 15:54:17 +1100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,11 @@
+#define WIFI_SSID     "????" // Add wifi name
+#define WIFI_PASSWORD "????" // Add wifi passowrd
+
+#define ID "air_quality" // Add unique name for this sensor
+#define INTERVAL 60 //seconds
+
+#define GC_PORT 443
+#define GC_PROM_URL "prometheus-prod-09-prod-au-southeast-0.grafana.net"
+#define GC_PROM_PATH "/api/prom/push"
+#define GC_PROM_USER "????"
+#define GC_PROM_PASS "????"
